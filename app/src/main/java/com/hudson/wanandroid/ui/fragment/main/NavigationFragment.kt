e090049155fc8f7155f027bc1dd52af42bedfcae
Hudson <2216274479@qq.com>
2020-07-12 15:03:05 +0800

    调整生成的fragment包位置

--- app/src/main/java/com/hudson/wanandroid/ui/fragment/main/NavigationFragment.kt
+++ app/src/main/java/com/hudson/wanandroid/ui/fragment/main/NavigationFragment.kt
@@ -1,10 +1,11 @@
-package com.hudson.wanandroid
+package com.hudson.wanandroid.ui.fragment.main
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hudson.wanandroid.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TreeFragment.newInstance] factory method to
+ * Use the [NavigationFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TreeFragment : Fragment() {
+class NavigationFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class TreeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tree, container, false)
+        return inflater.inflate(R.layout.fragment_navigation, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class TreeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TreeFragment.
+         * @return A new instance of fragment NavigationFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TreeFragment().apply {
+            NavigationFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/hudson/wanandroid/ui/fragment/main/ProjectsFragment.kt
+++ app/src/main/java/com/hudson/wanandroid/ui/fragment/main/ProjectsFragment.kt
@@ -1,10 +1,11 @@
-package com.hudson.wanandroid
+package com.hudson.wanandroid.ui.fragment.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hudson.wanandroid.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [NavigationFragment.newInstance] factory method to
+ * Use the [ProjectsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class NavigationFragment : Fragment() {
+class ProjectsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class NavigationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation, container, false)
+        return inflater.inflate(R.layout.fragment_projects, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class NavigationFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment NavigationFragment.
+         * @return A new instance of fragment ProjectsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            NavigationFragment().apply {
+            ProjectsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
