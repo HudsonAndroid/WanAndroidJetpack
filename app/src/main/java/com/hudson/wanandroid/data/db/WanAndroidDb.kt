4d1f51bc9a058287d404609a31c75e29f736341b
Hudson <2216274479@qq.com>
2020-08-28 19:47:42 +0800

    增加数据库加密模块

--- app/src/main/java/com/hudson/wanandroid/data/db/WanAndroidDb.kt
+++ app/src/main/java/com/hudson/wanandroid/data/db/WanAndroidDb.kt
@@ -5,10 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.hudson.wanandroid.BuildConfig
 import com.hudson.wanandroid.data.entity.Article
 import com.hudson.wanandroid.data.entity.HotWord
+import com.hudson.wanandroid.data.entity.LoginUser
 import com.hudson.wanandroid.data.entity.PagingNextKey
 import com.hudson.wanandroid.data.entity.wrapper.DataWrapper
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 
 /**
  * Created by Hudson on 2020/7/14.
@@ -18,9 +22,10 @@ import com.hudson.wanandroid.data.entity.wrapper.DataWrapper
         DataWrapper::class,
         PagingNextKey::class,
         Article::class,
-        HotWord::class
+        HotWord::class,
+        LoginUser::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 // DbMemberConverters被PagingNextKey使用
@@ -30,10 +35,12 @@ abstract class WanAndroidDb: RoomDatabase(){
     abstract fun pagingNextKeyDao(): PagingNextKeyDao
     abstract fun articleDao(): ArticleDao
     abstract fun hotWordDao(): HotWordDao
+    abstract fun loginUserDao(): LoginUserDao
 
     companion object{
         @Volatile
         private var instance: WanAndroidDb? = null
+        private const val PASSWORD = "hudson_jetpack_particle"
 
         fun getInstance(context: Context): WanAndroidDb{
             return instance?: synchronized(this){
@@ -44,10 +51,21 @@ abstract class WanAndroidDb: RoomDatabase(){
         }
 
         private fun provideDb(context: Context): WanAndroidDb{
-            return Room
-                .databaseBuilder(context, WanAndroidDb::class.java, "wanandroid.db")
-                .fallbackToDestructiveMigration()
-                .build()
+            val dbName = "wanandroid.db"
+            return if(BuildConfig.DEBUG){
+                Room
+                    .databaseBuilder(context, WanAndroidDb::class.java, dbName)
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }else{
+                // 如果是正式环境下，加密数据库
+                // see https://github.com/sqlcipher/android-database-sqlcipher
+                val passphrase = SQLiteDatabase.getBytes(PASSWORD.toCharArray())
+                Room.databaseBuilder(context, WanAndroidDb::class.java, dbName)
+                    .openHelperFactory(SupportFactory(passphrase))
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
         }
     }
 }
