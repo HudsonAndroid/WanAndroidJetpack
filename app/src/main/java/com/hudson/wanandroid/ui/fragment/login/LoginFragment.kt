f9769d69e2f57737fc0e3dddca22f4105745477b
Hudson <2216274479@qq.com>
2020-08-31 14:25:25 +0800

    登录成功后页面自动销毁

--- app/src/main/java/com/hudson/wanandroid/ui/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/hudson/wanandroid/ui/fragment/login/LoginFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
@@ -14,6 +13,7 @@ import com.hudson.wanandroid.R
 import com.hudson.wanandroid.databinding.FragmentLoginBinding
 import com.hudson.wanandroid.di.Injectable
 import com.hudson.wanandroid.ui.util.autoCleared
+import com.hudson.wanandroid.ui.util.showToast
 import com.hudson.wanandroid.viewmodel.AccountModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -45,9 +45,10 @@ class LoginFragment : Fragment(), Injectable {
             lifecycleScope.launch {
                 val login = accountModel.login()
                 if(login.isSuccess()){
-                    Toast.makeText(context, "login success", Toast.LENGTH_LONG).show()
+                    requireActivity().finish()
+                    showToast(R.string.tips_login_success)
                 }else{
-                    Toast.makeText(context, login.errorMsg, Toast.LENGTH_LONG).show()
+                    showToast(if(login.errorMsg.isNullOrEmpty()) getString(R.string.tips_login_failed) else login.errorMsg)
                 }
             }
         }
